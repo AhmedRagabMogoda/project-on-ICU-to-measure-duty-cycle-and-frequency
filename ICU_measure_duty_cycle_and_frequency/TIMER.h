e6bd9bdf919437c65008ada512cc8373ffa68df8
Ahmed Mogoda <ahmedragabmogouda@gmail.com>
2024-08-27 16:12:01 +0300

    Update TIMER.h

--- ICU_measure_duty_cycle_and_frequency/TIMER.h
+++ ICU_measure_duty_cycle_and_frequency/TIMER.h
@@ -12,28 +12,6 @@
 #include <avr/interrupt.h>
 
 
-void TIMER0_normal_init_with_interrupt(void);
-
-void TIMER0_CTC_init_with_interrupt(void);
-
-void TIMER0_CTC_wave_nonpwm(void);
-
-void TIMER0_fast_pwm_wave(void);
-
-void TIMER0_phase_correct_pwm_wave(void);
-
-void TIMER2_normal_init_with_interrupt(void);
-
-void TIMER2_CTC_init_with_interrupt(void);
-
-void TIMER2_CTC_wave_nonpwm(void);
-
-void TIMER2_fast_pwm_wave(void);
-
-void TIMER2_phase_correct_pwm_wave(void);
-
-void TIMER1_fast_pwm_8bit_wave(void);
-
 void TIMER1_ICU_init(void);
 
 unsigned short TIMER1_ICU_raising(void);
